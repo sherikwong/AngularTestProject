8ca8efd0176bd73352247f719f98146b5f8ae688
Sheri Kwong <sherikwong@gmail.com>
2018-08-03 14:28:49 -0400

    Linked users component in routes and made data service files to communicate with API

---
+++ src/app/data.service.ts
@@ -0,0 +1,11 @@
+// Service is useful for placing reusable code throughout different components. The purpose of this specific data service is to communicate with our API.
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+}
